9f4bd820ef7979d2f76d9a7ef6b4b876d66abe32
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-24 14:54:43 -0600

    Refactor: #append & #count

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -6,16 +6,29 @@ class LinkedList
   end
 
   def append(data)
-    node = Node.new(data)
-    @head = node 
-  end
-
-  def count 
-    if @head != nil
-      1
+    if @head == nil
+      @head = Node.new(data) 
     else
-      0
+      present_node = @head 
+      while present_node.next_node != nil
+        present_node = present_node.next_node 
+      end
+      present_node.next_node = Node.new(data) 
     end
+  end
+  
+  def count 
+    tick_marks = 1
+    if @head == nil 
+      0 
+    else
+      present_node = @head 
+      while present_node.next_node != nil
+        present_node = present_node.next_node 
+        tick_marks += 1
+      end
+    end
+    tick_marks
   end
 
   def to_string
