0508e698ff46dc624de66cf11e33603899c400cb
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-24 13:26:38 -0600

    Add method tests for multiple nodes

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -14,22 +14,46 @@ RSpec.describe LinkedList do
     end
   end
 
-  describe 'behavior' do 
-    xit '#append' do 
+  describe '#append' do 
+    it '#append 1 node' do 
       @list.append('doop')
       expect(@list.head).to be_a(Node)
       expect(@list.head.data).to eq('doop')
       expect(@list.head.next_node).to eq(nil)
     end
+    
+    it '#append >1 node' do 
+      @list.append('doop')
+      @list.append('deep')
+      expect(@list.head.next_node).to be_a(Node)
+      expect(@list.head.next_node.data).to eq('deep')
+      expect(@list.count).to eq(2)
+    end
+  end
 
-    xit '#count' do
+  describe '#count' do 
+    it 'counts 1 node' do
       @list.append('doop')
       expect(@list.count).to eq(1)
     end
 
-    xit '#to_string' do 
+    it 'counts 2 nodes' do 
+      @list.append('doop')
+      @list.append('deep')
+      expect(@list.count).to eq(2)
+    end
+  end
+  
+  describe '#to_string' do 
+    it '#to_string with 1 node' do 
       @list.append('doop')
       expect(@list.to_string).to eq('doop')
+    end
+    
+    it '#to_string for multiple nodes' do
+      @list.append('doop')
+      @list.append('deep')
+      expect(@list.to_string).to eq('doop deep')
     end
   end
 end
