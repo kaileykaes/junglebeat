f283dee7faf894bcd0a5ff7be95cf3990755fc91
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-24 18:30:35 -0600

    Feat: #append

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -4,4 +4,11 @@ class JungleBeat
   def initialize
     @list = LinkedList.new
   end
+
+  def append(datas)
+    node_data = datas.split(' ')
+    node_data.map do |data|
+      @list.append(data)
+    end
+  end
 end
