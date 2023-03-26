8a049465ce332093c5cd6072c3b7f262670bd541
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-26 11:24:55 -0600

    Feat: #all #prepend

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -1,8 +1,10 @@
 class JungleBeat
   attr_reader :list
+              :sound
   
-  def initialize
+  def initialize(sound = '')
     @list = LinkedList.new
+    @sound = append(sound)
   end
 
   def append(datas)
@@ -21,10 +23,22 @@ class JungleBeat
     `say -r 500 -v boing #{music}`
   end
 
+  def all
+    @list.to_string
+  end
+
+  def prepend(datas)
+    node_data = datas.split(' ')
+    node_data.map do |data|
+      @list.prepend(data) if validate?(data)
+    end
+  end
+
   #helpers
-  
+
   def validate?(data)
-    ['crack', 'woo', 'hoo', 'shu', 'deep', 'ditt', 
-      'frack', 'doo', 'click', 'doop', 'gof', 'pof'].include?(data)
+    ['crack', 'woo', 'tee', 'hoo', 'shu', 
+      'deep', 'ditt', 'frack', 'doo', 
+      'click', 'doop', 'gof', 'pof'].include?(data)
   end
 end
