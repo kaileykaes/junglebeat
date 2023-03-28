a82be5f6ccc0f2a8a30f2984f426cce050b33cc7
Kailey Kaes <56466345+kaileykaes@users.noreply.github.com>
2023-03-27 19:36:34 -0600

    Refactor: #play returns list count

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -17,15 +17,15 @@ class JungleBeat
     node_data.map do |data|
       @list.append(data) if validate?(data)
     end
-    @list.count
   end
-
+  
   def count
     @list.count
   end
-
+  
   def play 
     `say -r #{@rate} -v #{@voice} #{all}`
+    @list.count
   end
 
   def all
